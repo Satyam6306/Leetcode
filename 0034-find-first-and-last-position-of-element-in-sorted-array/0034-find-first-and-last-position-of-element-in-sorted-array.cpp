c92f81dbfa121f0342e75d1b448cfc2e97c8e440
Satyam6306 <srivastavasatyam806@gmail.com>
2026-07-31 17:25:12 +0530

    Time: 0 ms (100%), Space: 17.4 MB (90.09%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,48 @@
 class Solution {
 public:
-      int FindLeftMost(vector<int>& nums, int target, int n) {
-      int l=0;
-      int h=n-1;
 
-      int left_most=-1;
-
-      while(l<=h){
-        int mid =l+(h-l)/2;
-        if(nums[mid]==target){
-            left_most=mid;
-            h=mid-1;
-        }
-            else if(nums[mid]<target) {
-                l=mid+1;
-            }
-            else 
-            h=mid-1;
-        
-      }
-      return left_most;
-    }
-    int FindRightMost(vector<int>& nums, int target, int n) {
-
+    int Findlm(vector<int> &nums,int target,int n) {
         int l=0;
+        int h=n-1;
+        int left_most=-1;
+        while(l<=h) {
+            int mid=l+(h-l)/2;
+            cout<<mid<<endl;
+            if(nums[mid]==target){
+                left_most= mid;
+                h=mid-1;
+            } else if(nums[mid]<target){
+                l=mid+1;
+            } else 
+                h=mid-1;
+        }
+        return left_most;
+    }
+    int Findrm(vector<int> &nums,int target,int n) {
+        int l =0;
         int h=n-1;
 
         int right_most=-1;
-
         while(l<=h) {
-            int mid =l+(h-l)/2;
+            int mid=l+(h-l)/2;
             if(nums[mid]==target) {
                 right_most=mid;
                 l=mid+1;
-
             }
             else if(nums[mid]<target){
-                    l=mid+1;
+                l=mid+1;
             }
             else 
-            h=mid-1;
+                h=mid-1;
         }
         return right_most;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-       int n =nums.size();
-       int left_most= FindLeftMost(nums,target,n);
-       int right_most= FindRightMost(nums,target,n);
+        int n =nums.size();
 
-       return {left_most , right_most};
+        int left_most=Findlm(nums,target,n);
+        int right_most=Findrm(nums,target,n);
+        return {left_most,right_most};
     }
+    
 };
