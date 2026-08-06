b20761ea4b826e93afca1b49cb9844e63744344e
Satyam6306 <srivastavasatyam806@gmail.com>
2026-08-06 09:51:45 +0530

    Time: 0 ms (100%), Space: 12.4 MB (25.04%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+    sort(citations.begin(),citations.end());
+    int n =citations.size();
+
+    int l=0;
+    int h=n-1;
+
+    int ans=0;
+
+    while(l<=h){
+        int mid =l+(h-l)/2;
+cout<<mid;
+        if(citations[mid] >= n-mid) {
+             ans = max(ans, n - mid);
+                h = mid - 1;
+
+        }
+        else 
+            l=mid+1;
+        
+    }
+    
+    return ans;
+     
+    }
+};
